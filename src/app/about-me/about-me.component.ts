10ad23e3cc859cf9583e78c8505dcc947bd5ade2
Darylzz <cheer_bo50@hotmail.com>
2024-11-11 16:18:41 +0700

    initial ui about me

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss',
 })
-export class AboutMeComponent {}
+export class AboutMeComponent implements OnInit {
+  experience: any;
+  experienceInYears: string;
+  CurrentDayOfWork: number = new Date().getTime();
+  firstDayOfWork: number = new Date('2023-05-01').getTime();
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const summaryExperience = this.CurrentDayOfWork - this.firstDayOfWork;
+    this.experience = Math.floor(summaryExperience / (1000 * 60 * 60 * 24));
+    const years = (this.experience / 365).toString();
+    const remainingDays = this.experience % 365;
+    const months = (remainingDays / 30).toFixed(1);
+    this.experienceInYears = `${years.split('.')[0]}.${
+      months.split('.')[0]
+    } years`;
+  }
+}
