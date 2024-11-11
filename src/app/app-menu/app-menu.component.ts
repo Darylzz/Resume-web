e5a589e728524e30bee36586f1f31f488593fa8d
Darylzz <cheer_bo50@hotmail.com>
2024-11-11 16:19:04 +0700

    adjust app-menu for responsive web app

--- src/app/app-menu/app-menu.component.ts
+++ src/app/app-menu/app-menu.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { MediaSize } from '../constant/responsive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-app-menu',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, CommonModule],
   templateUrl: './app-menu.component.html',
   styleUrl: './app-menu.component.scss',
 })
 export class AppMenuComponent {
+  currentWidth: number;
+  mediaSize = MediaSize;
+
+  @HostListener('window:resize', ['$event.target.innerWidth'])
+  onResize(width: number) {
+    this.currentWidth = width;
+  }
+
   constructor(private router: Router) {}
 
   onClickNavigateToHome() {
